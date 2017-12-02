850a605e1b8d46def3aeb0a97ccd6f50797d80de
Adam Morris <amorris@mistermorris.com>
2017-12-03 07:57:17 +0800

    migration

--- app/model.py
+++ app/model.py
@@ -11,11 +11,15 @@ class UOpenapplyIntegration(Base):
     __tablename__ = 'u_openapply_integration'
 
     id = Column(Numeric(11, 0, asdecimal=False), primary_key=True)
-    oa_id = Column(Numeric(25, 10))
-    oa_name = Column(String(40))
     whocreated = Column(String(100))
     whencreated = Column(DateTime)
     whomodified = Column(String(100))
     whenmodified = Column(DateTime)
     ps_student_number = Column(String(10))
-    status = Column(String(10), server_default="_")
+    status = Column(String(40), server_default="_")
+    passport_id = Column(String(40), server_default="")
+    first_name = Column(String(40))
+    last_name = Column(String(40))
+    address = Column(String(40), server_default="")
+    address_ii = Column(String(40), server_default="")
+    full_address = Column(String(40), server_default="")
